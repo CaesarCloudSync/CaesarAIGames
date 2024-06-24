6d66b3a9b3b56acaa9d0f3b916778ec1f383215a
amari <amari.lawal@gmail.com>
2024-06-24 14:52:58 +0100

    unzip works but its specific to linux

--- AmariZip/amarizip.py
+++ AmariZip/amarizip.py
@@ -1,27 +1,29 @@
-source_path = "/media/amari/SSD T7/steamunlockedgames/SIFU.v1.27.Incl.ALL.DLC.zip"
-target_path = "/media/amari/SSD T7/steamunlockedgames/SIFU.v1.27.Incl.ALL.DLC_PI"
+import zipfile
+import zlib
+import os
+from tqdm import tqdm
+doc = "/media/amari/SSD T7/steamunlockedgames/Sekiro.Shadows.Die.Twice.v1.06.zip"
+#doc = "/media/amari/SSD T7/steamunlockedgames/Kingdom.Come.Deliverance.v1.9.6.Incl.ALL.DLC.zip"
+from os import fspath
+from pathlib import Path
+from shutil import copyfileobj
 from zipfile import ZipFile
+from tqdm.auto import tqdm  # could use from tqdm.gui import tqdm
+from tqdm.utils import CallbackIOWrapper
 
-class ZipFileCustom(ZipFile):
-    """ Custom ZipFile class with a callback on extractall. """
+def extractall(fzip, dest, desc="Extracting"):
+    """zipfile.Zipfile(fzip).extractall(dest) with progress"""
+    dest = Path(dest).expanduser()
+    with ZipFile(fzip) as zipf, tqdm(
+        desc=desc, unit="B", unit_scale=True, unit_divisor=1024,
+        total=sum(getattr(i, "file_size", 0) for i in zipf.infolist()),
+    ) as pbar:
+        for i in zipf.infolist():
+            if not getattr(i, "file_size", 0):  # directory
+                zipf.extract(i, fspath(dest))
+            else:
+                with zipf.open(i) as fi, open(fspath(dest / i.filename), "wb") as fo:
+                    copyfileobj(CallbackIOWrapper(pbar.update, fi), fo)
 
-    def extractall(self, path=None, members=None, pwd=None, fn_progress=None):
-        if members is None:
-            members = self.namelist()
-
-        if path is None:
-            path = os.getcwd()
-        else:
-            path = os.fspath(path)
-
-        for index, member in enumerate(members):
-            print(member)
-            if fn_progress:
-                fn_progress(len(members), index + 1)
-            self._extract_member(member, path, pwd)
-def calculate_percentage(total: int, current: int):
-    percent = float(current) / total
-    percent = round(percent * 100)
-    return percent
-with ZipFileCustom(source_path) as zfc:
-    zfc.extractall(target_path, fn_progress=lambda total, current: print(f'{calculate_percentage(total, current)} %'))
+with open(doc,"rb") as zf:
+    extractall(zf,"/media/amari/SSD T7/steamunlockedgames/Sekiro.Shadows.Die.Twice.v1.06_Test")
